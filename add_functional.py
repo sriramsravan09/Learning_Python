5b0640f8c8a8ccc5e21e11f358bdad9e9622f969
sravan <sriramsravan09@gmail.com>
2022-10-06 21:04:52 +1100

    initial version of arthemetic program

--- add_functional.py
+++ add_functional.py
@@ -1,9 +1,9 @@
 #get the first number
-def add(x,y):
-    x=20
-    #get the second number
-    y=30
-    #add two numbers abd store the result
-    z=x+y
-    #print(z)
+def add_numbers(x,y):
+    x = 18
+    y = 20
+    z = x+y
     print(z)
+    return z
+
+add_numbers(18,20)

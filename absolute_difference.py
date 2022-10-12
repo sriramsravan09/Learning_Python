3db1de3ef9caf17933923f84f88cf31d44c7688f
sravan <sriramsravan09@gmail.com>
2022-10-12 17:19:04 +1100

    absolute difference

---
+++ absolute_difference.py
@@ -0,0 +1,14 @@
+# get the first number
+# print(*enter first input*)
+x = input("enter first input")
+print("first value is" + x)
+print("enter second input")
+y = input("enter second input")
+print("second value is" + y)
+# sub the numbers and store the results
+print("results")
+z = (int(x) - int(y))
+if z < 0:
+    print(-z)
+else:
+    print(z)

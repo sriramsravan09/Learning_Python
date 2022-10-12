2005e7da56c56db611fe43068be258af54c7236c
sravan <sriramsravan09@gmail.com>
2022-10-12 18:38:01 +1100

    sub_functional

--- sub_funcational.py
+++ sub_funcational.py
@@ -1,9 +1,14 @@
-#get the first number
-def sub_numbers(x,y):
-    x = 18
-    y = 20
-    z = x-y
+# get the first number
+# print(*enter first input*)
+x = input("enter first input")
+print("first value is" + x)
+print("enter second input")
+y = input("enter second input")
+print("second value is" + y)
+# sub the numbers and store the results
+print("results")
+z = (int(x) - int(y))
+if z < 0:
+    print(-z)
+else:
     print(z)
-    return z
-
-sub_numbers(18,20)

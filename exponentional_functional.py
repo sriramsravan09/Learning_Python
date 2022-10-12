d420b8e13189613e021e8c87b69c85dad146ae20
sravan <sriramsravan09@gmail.com>
2022-10-12 17:56:09 +1100

    exponentional_functional

--- exponentional_functional.py
+++ exponentional_functional.py
@@ -1,9 +1,14 @@
-#get the first number
-def exponentional_numbers(x,y):
-    x = 180
-    y = 2
-    z = x**y
+# get the first number
+# print(*enter first input*)
+x = input("enter first input")
+print("first value is" + x)
+print("enter second input")
+y = input("enter second input")
+print("second value is" + y)
+# sub the numbers and store the results
+print("results")
+z = (int(x)  ** int(y))
+if z < 0:
+    print(-z)
+else:
     print(z)
-    return z
-
-exponentional_numbers(180,2)

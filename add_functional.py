c8eb708deeae152c760cb3cd54a17e32f60ed1dd
sravan <sriramsravan09@gmail.com>
2022-10-12 20:39:38 +1100

    add_functional

--- add_functional.py
+++ add_functional.py
@@ -1,14 +1,12 @@
 # get the first number
 # print(*enter first input*)
-x = input("enter first input")
-print("first value is" + x)
-print("enter second input")
-y = input("enter second input")
-print("second value is" + y)
-# add the numbers and store the results
-print("results")
-z = (int(x) + int(y))
-if z < 0:
-    print(-z)
-else:
-    print(z)
+def add(a, b):
+    sum = a + b
+    return sum
+# Taking input
+num1 = int(input("first number: "))
+num2 = int(input("second number: "))
+# call function
+sum = add(num1, num2)
+# result
+print("Sum of two numbers is: ", sum)

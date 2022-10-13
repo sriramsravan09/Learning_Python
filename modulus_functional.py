27ffa3ade5581a7b555b21472d64a240b27ff2d7
sravan <sriramsravan09@gmail.com>
2022-10-13 19:23:17 +1100

    modulus_functional 13/10/22

--- modulus_functional.py
+++ modulus_functional.py
@@ -1,11 +1,23 @@
-# get the first number
-# print(*enter first input*)
-x = input("enter first input")
-print("first value is" + x)
-print("enter second input")
-y = input("enter second input")
-print("second value is" + y)
-# modulue the numbers and store the results
-print("results")
-z = (int(x) % int(y))
-print(z)
+def modulus(x, y):
+    modulus = int(p) % int(q)
+    print(modulus)
+
+
+print("first input")
+a = input()
+if int(a) < 0:
+    print("not valid")
+    exit()
+else:
+    print("first number is " + a)
+print("second input")
+b = input()
+if int(b) < 0:
+    print("not valid ")
+    exit()
+else:
+    print("second number is " + b)
+    print("result")
+modulus = int(a) // int(b)
+
+print(modulus)

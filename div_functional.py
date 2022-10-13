e810c0820b4d3b394b358225eb834d37e5bc99b1
sravan <sriramsravan09@gmail.com>
2022-10-13 19:05:29 +1100

    div_functional 13/10/22

--- div_functional.py
+++ div_functional.py
@@ -1,11 +1,23 @@
-# get the first number
-# print(*enter first input*)
-x = input("enter first input")
-print("first value is" + x)
-print("enter second input")
-y = input("enter second input")
-print("second value is" + y)
-# div the numbers and store the results
-print("results")
-z = (int(x) / int(y))
-print(z)
+def div(x, y):
+    div = int(p) / int(q)
+    print(div)
+
+
+print("first input")
+a = input()
+if int(a) < 0:
+    print("not valid")
+    exit()
+else:
+    print("first number is " + a)
+print("second input")
+b = input()
+if int(b) < 0:
+    print("not valid ")
+    exit()
+else:
+    print("second number is " + b)
+    print("result")
+div = int(a) / int(b)
+
+print(div)

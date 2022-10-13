a7e3f4809efed3b452cd0d3d83980503749aa44f
sravan <sriramsravan09@gmail.com>
2022-10-13 21:26:21 +1100

    add_proc

---
+++ add_proc(+).py
@@ -0,0 +1,18 @@
+print("1st input")
+a = input()
+if int(a) < 0:
+    print("na")
+    exit()
+else:
+    print("1st num is " + a)
+print("2nd input")
+b = input()
+if int(b) < 0:
+    print("na")
+    exit()
+else:
+    print("2nd num is " + b)
+
+add = int(a) + int(b)
+
+print(add)

e6ada8fd96e15dd4a55f476e319fcff40dbb0082
sravan <sriramsravan09@gmail.com>
2022-10-20 21:40:29 +1100

    leap year_functional

--- leap year_procedural.py
+++ leap year_procedural.py
@@ -1,13 +1,21 @@
 # calculating leap year
 # I want title as leap year
 print("leap year")
+
 # give year int input since it is a number
 year = int(input("enter year:  "))
+
 # logic to find leap year is first, number should be evenly divided by 4 and
 # same time it should evenly divided by 100 and quotient should not be a float
 # then check if it is evenly dividing by 400
-if (year % 4 == 0 and year % 100 != float) or year % 400 == 0:
+
+if year % 400 == 0:
+    print("yes1")
+elif year % 100 != 0:
+    print("yes2")
+elif (year % 4 == 0 ):
+    print("yes3")
+
     # maintain indentation and type how your output wants to look
-    print("yes")
 else:
     print("no")

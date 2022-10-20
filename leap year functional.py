9d966cdc9297d1514d565fe3b8f76b2d30411f05
sravan <sriramsravan09@gmail.com>
2022-10-20 13:48:02 +1100

    leap year_functional

---
+++ leap year functional.py
@@ -0,0 +1,12 @@
+def leapyear(x):
+    return (x % 4 == 0 and x % 100 != float) or x % 400 == 0
+    print("leapyear")
+
+x = int(input("year:  "))
+if x <= 0:
+    print("year cannot be negative")
+elif (leapyear(x)):
+ print("hurry it is a leap year")
+else:
+ print("oops it is a not a leap year")
+

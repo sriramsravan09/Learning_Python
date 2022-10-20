7da57fd764080eb4819b53fa1444ea54a975d06e
sravan <sriramsravan09@gmail.com>
2022-10-20 15:59:44 +1100

    absolute diff functional

--- absu_diff_functional.py
+++ absu_diff_functional.py
@@ -1,27 +1,15 @@
 def abb_diff(x, y):
     abb_diff = int(x) - int(y)
-    print(abb_diff)
+    if abb_diff < 0:
+        print(-abb_diff)
+    else:
+        print(abb_diff)
+x=int(input("1st num     "))
+y=int(input("2nd num    "))
+print("results are")
+abb_diff(x,y)
 
 
-print("first input")
-a = input()
-if int(a) < 0:
-    print("not valid")
-    exit()
-else:
-    print("first number is " + a)
-print("second input")
-b = input()
-if int(b) < 0:
-    print("not valid ")
-    exit()
-else:
-    print("second number is " + b)
-    print("result")
-abb_diff = int(a) - int(b)
-if abb_diff < 0:
-    print(-abb_diff)
-else:
-    print(abb_diff)
+
 
 

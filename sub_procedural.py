0a6fa54a99ee0c0f9c1e79c261cd7bb6229f9ec5
sravan <sriramsravan09@gmail.com>
2022-10-22 20:18:39 +1100

    sub_functional.py

--- sub_procedural.py
+++ sub_procedural.py
@@ -1,18 +1,20 @@
-print("1st input")
+print("1st num")
 a = input()
 if int(a) < 0:
     print("na")
     exit()
 else:
-    print("1st num is " + a)
-print("2nd input")
-b = input()
-if int(b) < 0:
-    print("na")
-    exit()
-else:
-    print("2nd num is " + b)
+    print("1st num is  " + a)
 
-sub = int(a) - int(b)
 
-print(sub)
+    print("2nd num")
+    b = input()
+    if  int(b) < 0:
+        print("na")
+        exit()
+    else:
+        print("2nd num is   " + b)
+
+        sub = int(a)-int(b)
+        print("results are")
+        print(sub)

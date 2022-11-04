cc3dbbd69ec12caadaee78fd335ee58b4ac0c052
sravan <sriramsravan09@gmail.com>
2022-11-04 12:08:10 +1100

    string_indexing_operations

--- string_indexing_operations.py
+++ string_indexing_operations.py
@@ -1,20 +1,39 @@
 # Indexing Operator []
 
 # you are going to learn Indexing now
-
-x=str(input("enter a string value"))
+#. Program should accept a string value while running
+x=str(input("enter a string value=    "))
+print("program is acceped the value you entered because it is string")
 print(x)
-print("you entered", x)
-if len(x) % 2 ==0:
- #print ("your string is even")
- print("first charecter in", x ," is...", x[0])
- print("last charecter in ",x, "is...",x[-1] )
- print("length of the string", x,"is",len(x))
- print("middle index is", len(x)/2)
+print("you entered   ", x)
+# Find out first character in the string using string index
+
+print("first charecter in", x, "is", x[0])
+
+# Find out last character in the string using string index
+
+print("last charecter in", x, "is", x[-1])
+
+# Find out length of the string using “len” function and print the result
+
+print("length of the string", x, "is", len(x))
+
+# Find out the middle character in the string using index
+
+print("mid index is", len(x) // 2)
+
+#a. If length of the string is an odd number print the middle character.
+
+if len(x) % 2 != 0:
  print("middle character in ", x, "is", x[int(len(x) / 2)])
+
+# b. If length of the string in an even number then print two of the middle characters
 else:
- print("first charecter in", x, "is...", x[0])
- print("last charecter in", x, "is...", x[-1])
- print("length of the string", x, "is", len(x))
- print("middle index is", len(x)//2)
- print("middle character in", x, "is", x[int(len(x) //2)])
+ print("middle characters in", x, "is", x[int(len(x) // 2)], "and", x[int(len(x) // 2) + 1])
+
+
+
+
+
+
+

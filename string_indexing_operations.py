70ea2f8a3b6ceb90e426a2815a8d40896d2df4ca
sravan <sriramsravan09@gmail.com>
2022-11-04 13:11:23 +1100

    string_slicing_operations

--- string_indexing_operations.py
+++ string_indexing_operations.py
@@ -5,18 +5,18 @@
 x=str(input("enter a string value=    "))
 print("program is acceped the value you entered because it is string")
 print(x)
-print("you entered   ", x)
+print("you entered:   ", "'",x,"'")
 # Find out first character in the string using string index
 
-print("first charecter in", x, "is", x[0])
+print("first charecter in   '",x,"' is", "'",x[0],"'")
 
 # Find out last character in the string using string index
 
-print("last charecter in", x, "is", x[-1])
+print("last charecter in   '",x,"'is", "'",x[-1],"'")
 
 # Find out length of the string using “len” function and print the result
 
-print("length of the string", x, "is", len(x))
+print("length of the string   '",x,"'is", len(x))
 
 # Find out the middle character in the string using index
 
@@ -25,11 +25,11 @@ print("mid index is", len(x) // 2)
 #a. If length of the string is an odd number print the middle character.
 
 if len(x) % 2 != 0:
- print("middle character in ", x, "is", x[int(len(x) / 2)])
+ print("middle character in   '",x,"'is", "'",x[int(len(x) / 2)],"'")
 
 # b. If length of the string in an even number then print two of the middle characters
 else:
- print("middle characters in", x, "is", x[int(len(x) // 2)], "and", x[int(len(x) // 2) + 1])
+ print("middle characters in   '",x,"' are", "'",x[int(len(x) // 2)],"'", "and", "'",x[int(len(x) // 2) + 1],"'")
 
 
 

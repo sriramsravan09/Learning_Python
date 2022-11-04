964388578a764b2e7989d41cd84851bd6b7f65b1
sravan <sriramsravan09@gmail.com>
2022-11-04 13:16:03 +1100

    string_slicing_operations

--- string_slicing_operations.py
+++ string_slicing_operations.py
@@ -6,22 +6,22 @@ print("You entered:",x)
 #find out the slice of that string after removing first and last character
 y=len(x)
 x1=x[1:-1]
-print("Slice of", x, "after removing first and last character is",x1)
+print("Slice of", "'",x,"'", "after removing first and last character is","  '",x1,"'")
 
 #.Find out the slice of the string after removing the first 2 characters
 
 
 x2=x[2:]
-print("Slice of", x,"after removing the first 2 characters is",x2)
+print("Slice of", "'",x,"'","after removing the first 2 characters is","  '",x2,"'")
 
 #Find out the slice of the string after removing the last 2 characters
 
 x3=x[:-2]
-print("Slice of the",x, "after removing the last 2 characters is",x3)
+print("Slice of the","'",x,"'", "after removing the last 2 characters is","  '",x3,"'")
 
 #Slice the string in the middle and display both strings.
 print("Mid index is", len(x) // 2)
 x4=x[:int(len(x)//2)]
 x5=x[int(len(x)//2):]
-print("First half slice of",x, "is",x4)
-print("Second half slice of",x, "is",x5)
+print("First half slice of","'",x,"'", "is","  '",x4,"'")
+print("Second half slice of","'",x,"'", "is","  '",x5,"'")

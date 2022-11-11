6f713490fd22e2e750c2a0260a6f61bae3cda85d
sravan <sriramsravan09@gmail.com>
2022-11-11 17:42:51 +1100

    odd_loop

--- odd_numbers_loop.py
+++ odd_numbers_loop.py
@@ -2,10 +2,12 @@ A=int(input("Enter a number"))
 print(" You entered  '",A,"'")
 B=int(input("How many odd numbers you want to print"))
 print("printing", B, "odd numbers after", A)
-
-for i in range(A,B+1):
+C=2*B-1
+for i in range(A,C):
  if i % 2 != 0:
   print(i)
+
+
 
 
 
